912d69398ab09158fcda982b204a3955d1c65581
bkoltai <koltaibenji@gmail.com>
2017-01-22 11:53:33 -0800

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # foo
+
+## Install Ruby
+1. [Install rbenv](https://github.com/rbenv/rbenv#homebrew-on-mac-os-x)
+1. Install ruby 2.3.1 `rbenv install 2.3.1 && rbenv rehash && rbenv local 2.3.1`
+1. `gem install sinatra twilio-ruby`
+
---
+++ twiml-quickstart.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+require 'twilio-ruby'
+
+get '/hello-monkey' do
+  people = {
+    '+14158675309' => 'Curious George',
+    '+14158675310' => 'Boots',
+    '+14158675311' => 'Virgil',
+    '+14158675312' => 'Marcel',
+  }
+  name = people[params['From']] || 'Monkey'
+  Twilio::TwiML::Response.new do |r|
+    r.Say "Hello #{name}"
+  end.text
+end
